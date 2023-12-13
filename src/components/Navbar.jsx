069fa3b09f735d2f2e4259727579ed8d516173b0
Roland Kamga <106543063+Kamga-Roland@users.noreply.github.com>
2023-12-13 20:02:29 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
